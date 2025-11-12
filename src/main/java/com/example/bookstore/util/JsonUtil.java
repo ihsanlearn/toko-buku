bc4f4d839a528fb56f390d0ca227363280977b9a
ihsanlearn <iihsannlearn@gmail.com>
2025-11-12 13:24:06 +0700

    mulai kerja syg

---
+++ src/main/java/com/example/bookstore/util/JsonUtil.java
@@ -0,0 +1,21 @@
+package com.example.bookstore.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.bookstore.model.Book;
+
+import java.io.File;
+import java.util.List;
+
+public class JsonUtil {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static List<Book> loadBooks(File file) throws Exception {
+        if (!file.exists()) return List.of();
+        return mapper.readValue(file, new TypeReference<>() {});
+    }
+
+    public static void saveBooks(File file, List<Book> books) throws Exception {
+        mapper.writerWithDefaultPrettyPrinter().writeValue(file, books);
+    }
+}

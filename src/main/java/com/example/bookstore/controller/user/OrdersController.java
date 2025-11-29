6bf5127b1d385a5cf5ebdfd58ae44e30387ef0f7
ihsanlearn <iihsannlearn@gmail.com>
2025-11-29 16:33:44 +0700

    add order list feature

--- src/main/java/com/example/bookstore/controller/user/OrdersController.java
+++ src/main/java/com/example/bookstore/controller/user/OrdersController.java
@@ -10,17 +10,24 @@ import com.example.bookstore.session.SessionManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.time.LocalDateTime;
 
 public class OrdersController {
 
-    @FXML private TableView<?> tableOrders;
-    @FXML private TableColumn<?, ?> colOrderId;
-    @FXML private TableColumn<?, ?> colBook;
-    @FXML private TableColumn<?, ?> colDate;
-    @FXML private TableColumn<?, ?> colStatus;
+    @FXML
+    private TableView<Transaction> tableOrders;
+    @FXML
+    private TableColumn<Transaction, Integer> colOrderId;
+    @FXML
+    private TableColumn<Transaction, Integer> colBook;
+    @FXML
+    private TableColumn<Transaction, LocalDateTime> colDate;
+    @FXML
+    private TableColumn<Transaction, String> colStatus;
 
     private TransactionService transactionService = new TransactionService();
-    
+
     private List<Transaction> currentUserTransaction;
     private User currentUser;
 
@@ -29,7 +36,11 @@ public class OrdersController {
         currentUser = SessionManager.getCurrentUser();
         currentUserTransaction = transactionService.getTransactionsByUserId(currentUser.getId());
 
-        
+        colOrderId.setCellValueFactory(new PropertyValueFactory<>("transactionId"));
+        colBook.setCellValueFactory(new PropertyValueFactory<>("bookId"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("purchaseDate"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        tableOrders.getItems().setAll(currentUserTransaction);
     }
 }
-

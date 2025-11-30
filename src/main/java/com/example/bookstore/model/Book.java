2d694735001b108cf46059b8170564e5a3a15a06
ihsanlearn <iihsannlearn@gmail.com>
2025-11-30 08:09:34 +0700

    update books model

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -1,72 +1,111 @@
-    package com.example.bookstore.model;
+package com.example.bookstore.model;
 
-    public class Book {
+public class Book {
 
-        private int id;
-        private String title;
-        private String author;
-        private int price;
-        private int stock;
-        private String imgPath;
+    private int id;
+    private String title;
+    private String author;
+    private int price;
+    private int stock;
+    private String imgPath;
+    private int discount;
+    private int soldCount;
+    private String category;
+    private String description;
 
-        public Book(String title, String author, int price, int stock, String imgPath) {
-            this.title = title;
-            this.author = author;
-            this.price = price;
-            this.stock = stock;
-            this.imgPath = imgPath;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getImgPath() {
-            return this.imgPath;
-        }
-
-        public void setImgPath(String imgPath) {
-            this.imgPath = imgPath;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public void setPrice(int price) {
-            this.price = price;
-        }
-
-        public int getStock() {
-            return stock;
-        }
-
-        public void setStock(int stock) {
-            this.stock = stock;
-        }
-
-        @Override
-        public String toString() {
-            return title + " - " + author;
-        }
+    public Book(String title, String author, int price, int stock, String imgPath, String category,
+            String description) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+        this.imgPath = imgPath;
+        this.category = category;
+        this.description = description;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgPath() {
+        return this.imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getSoldCount() {
+        return soldCount;
+    }
+
+    public void setSoldCount(int soldCount) {
+        this.soldCount = soldCount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + author;
+    }
+}
